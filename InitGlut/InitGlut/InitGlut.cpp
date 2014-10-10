6a1632ded0eeedc0828bd1e2ec37592ca30f9e82
lzngame <474165841@qq.com>
2014-10-10 20:13:55 +0800

    add some explannatory note

--- InitGlut/InitGlut/InitGlut.cpp
+++ InitGlut/InitGlut/InitGlut.cpp
@@ -8,6 +8,7 @@ void render(void){
 	//清除上次显示结果，每次绘制前调用
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClearColor(0.0,1.0,0.0,0.0);
+	//世界窗口以当前窗口中点为原点，向右为+x轴，向上为+y轴
 	glBegin(GL_TRIANGLES);
 		glColor3f(1.0,1.0,1.0);
 		glVertex3f(1.0,1.0,1.0);
@@ -24,6 +25,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	//glutInitDisplayMode(GLUT_DEPTH|GLUT_SINGLE|GLUT_RGBA);
 	glutInitDisplayMode(GLUT_DEPTH|GLUT_RGBA|GLUT_DOUBLE);
 	glutInitWindowPosition(-1,-1);
+	//实现的是比例缩放
 	glutInitWindowSize(400,400);
 	glutCreateWindow("OpenGL-glutInit()");
 	glutDisplayFunc(render);//寄存回调
